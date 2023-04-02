bb9a621a84ff33f32a1c0e32d566c3447fef355c
Nic Motta <nic.motta@accenture.com>
2023-04-02 16:36:04 -0300

    borrado librerias no usadas

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,11 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  // site: 'https://muru7-8.github.io',
-  // base: '/',
   integrations: [react(), svelte(), tailwind()]
 });

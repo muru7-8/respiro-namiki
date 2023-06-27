c699311a704d97ef3921390351bee926c966188f
Nic Motta <nic.motta@accenture.com>
2023-06-27 18:00:58 -0300

    pwa configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,24 +8,30 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
+// Helper imports
+import { manifest, seoConfig } from "./utils/seoConfig"
+
 // https://astro.build/config
 export default defineConfig({
+  site: seoConfig.baseURL,
   output: 'server',
   adapter: netlify(),
   integrations: [react(), svelte(), tailwind()],
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest,
-      workbox: {
-        globDirectory: 'dist',
-        globPatterns: [
-          '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-        ],
-      // Don't fallback on document based (e.g. `/some-page`) requests
-      // This removes an errant console.log message from showing up.
-      navigateFallback: null,
-      },
-    })
-  ]
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest,
+        workbox: {
+          globDirectory: 'dist',
+          globPatterns: [
+            '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+          ],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null,
+        },
+      })
+    ]
+  }
 });

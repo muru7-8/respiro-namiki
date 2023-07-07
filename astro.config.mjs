e7b21e01ea83bcc599e0331d30915cdd8b9e8672
Nic Motta <nic.motta@accenture.com>
2023-07-07 13:29:11 -0300

    rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-import { VitePWA } from 'vite-plugin-pwa'
 import AstroPWA from '@vite-pwa/astro'
 
 import tailwind from "@astrojs/tailwind";

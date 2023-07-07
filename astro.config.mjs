b85f2fef5cb7814c6c5b1587b6599a5c8813416e
Nic Motta <nic.motta@accenture.com>
2023-07-07 13:35:55 -0300

    rollback 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,59 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-import AstroPWA from '@vite-pwa/astro'
-
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
-
-// Helper imports
-import { manifest, seoConfig } from "./utils/seoConfig"
 
 // https://astro.build/config
 export default defineConfig({
-  site: seoConfig.baseURL,
   output: 'server',
   adapter: netlify(),
-  integrations: [react(), svelte(), tailwind(),
-    AstroPWA({
-      mode: 'development',
-      base: '/',
-      scope: '/',
-      includeAssets: ['favicon.svg'],
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Astro PWA',
-        short_name: 'Astro PWA',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-      workbox: {
-        navigateFallback: '/404',
-        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
-      },
-      devOptions: {
-        enabled: true,
-        navigateFallbackAllowlist: [/^\/404$/],
-      },
-    }),
-  ],
+  integrations: [react(), svelte(), tailwind()],
 });

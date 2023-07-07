08f83e6ca151d3c4cb9dda1fc9459e5a9f2b7fff
Nic Motta <nic.motta@accenture.com>
2023-07-07 13:25:34 -0300

    test pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import { VitePWA } from 'vite-plugin-pwa'
+import AstroPWA from '@vite-pwa/astro'
 
 import tailwind from "@astrojs/tailwind";
 
@@ -16,22 +17,44 @@ export default defineConfig({
   site: seoConfig.baseURL,
   output: 'server',
   adapter: netlify(),
-  integrations: [react(), svelte(), tailwind()],
-  vite: {
-    plugins: [
-      VitePWA({
-        registerType: "autoUpdate",
-        manifest,
-        workbox: {
-          globDirectory: 'dist',
-          globPatterns: [
-            '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-          ],
-        // Don't fallback on document based (e.g. `/some-page`) requests
-        // This removes an errant console.log message from showing up.
-        navigateFallback: null,
-        },
-      })
-    ]
-  }
+  integrations: [react(), svelte(), tailwind(),
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Astro PWA',
+        short_name: 'Astro PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/404',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/404$/],
+      },
+    }),
+  ],
 });
